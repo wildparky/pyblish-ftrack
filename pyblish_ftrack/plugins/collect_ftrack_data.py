23aabffd37d74cbc640d3ea77ba870da2f18334d
Milan Kolar <mail@mkolar.com>
2015-12-22 13:17:03 +0100

    save data as ordered dictionary and add more flexibility for possible ftrack workflows.

--- pyblish_ftrack/plugins/collect_ftrack_data.py
+++ pyblish_ftrack/plugins/collect_ftrack_data.py
@@ -10,8 +10,6 @@ import pyblish.api
 class CollectFtrackData(pyblish.api.Selector):
 
     """Collects ftrack data from FTRACK_CONNECT_EVENT
-        Arguments:
-            version (int): version number of the publish
     """
 
     order = pyblish.api.Selector.order
@@ -34,79 +32,25 @@ class CollectFtrackData(pyblish.api.Selector):
 
         task = ftrack.Task(taskid)
 
-        ftrack_data = self.get_context(task)
+        ftrack_data = self.get_data(task)
 
         # set ftrack data
         context.data['ftrackData'] = dict(ftrack_data)
 
         self.log.info('Found ftrack data: \n\n%s' % ftrack_data)
 
-    # def get_data(self, taskid):
-    #
-    #     task_codes = {
-    #         'Animation': 'anim',
-    #         'Layout': 'layout',
-    #         'FX': 'fx',
-    #         'Compositing': 'comp',
-    #         'Motion Graphics': 'mograph',
-    #         'Lighting': 'light',
-    #         'Modeling': 'geo',
-    #         'Rigging': 'rig',
-    #         'Art': 'art',
-    #     }
-    #
-    #     try:
-    #         task = ftrack.Task(id=taskid)
-    #     except ValueError:
-    #         task = None
-    #
-    #     parents = task.getParents()
-    #     project = ftrack.Project(task.get('showid'))
-    #     taskType = task.getType().getName()
-    #     entityType = task.getObjectType()
-    #
-    #     ctx = {
-    #         'Project': {
-    #             'name': project.get('fullname'),
-    #             'code': project.get('name'),
-    #             'id': task.get('showid'),
-    #             'root': project.getRoot(),
-    #         },
-    #         entityType: {
-    #             'type': taskType,
-    #             'name': task.getName(),
-    #             'id': task.getId(),
-    #             'code': task_codes.get(taskType, None)
-    #         }
-    #     }
-    #
-    #     for parent in parents:
-    #         tempdic = {}
-    #         if parent.get('entityType') == 'task' and parent.getObjectType():
-    #             objectType = parent.getObjectType()
-    #             tempdic['name'] = parent.getName()
-    #             tempdic['description'] = parent.getDescription()
-    #             tempdic['id'] = parent.getId()
-    #             if objectType == 'Asset Build':
-    #                 tempdic['type'] = parent.getType().get('name')
-    #                 objectType = objectType.replace(' ', '_')
-    #
-    #             ctx[objectType] = tempdic
-    #
-    #     return ctx
-
-    def get_context(self, entity):
+    def get_data(self, entity):
 
         task_codes = {
-                'Animation': 'anim',
-                'Layout': 'layout',
-                'FX': 'fx',
-                'Compositing': 'comp',
-                'Motion Graphics': 'mograph',
-                'Lighting': 'light',
-                'Modeling': 'geo',
-                'Rigging': 'rig',
-                'Art': 'art',
+            'Animation': 'anim',
+            'Layout': 'layout',
+            'FX': 'fx',
+            'Compositing': 'comp',
+            'Motion Graphics': 'mograph',
+            'Lighting': 'light',
+            'Modeling': 'geo',
+            'Rigging': 'rig',
+            'Art': 'art',
             }
 
         entityName = entity.getName()
@@ -116,7 +60,7 @@ class CollectFtrackData(pyblish.api.Selector):
 
         hierarchy = entity.getParents()
 
-        ctx = collections.OrderedDict()
+        data = collections.OrderedDict()
 
         if entity.get('entityType') == 'task' and entityType == 'Task':
             taskType = entity.getType().getName()
@@ -134,7 +78,7 @@ class CollectFtrackData(pyblish.api.Selector):
                 'description': entityDescription
             }
 
-        ctx[entityType] = entityDic
+        data[entityType] = entityDic
 
         for ancestor in hierarchy:
             tempdic = {}
@@ -158,7 +102,8 @@ class CollectFtrackData(pyblish.api.Selector):
             elif objectType == 'Project':
                 tempdic['code'] = tempdic['name']
                 tempdic['name'] = ancestor.get('fullname')
+                tempdic['root'] = ancestor.getRoot(),
 
-            ctx[objectType] = tempdic
+            data[objectType] = tempdic
 
-        return ctx
+        return data
